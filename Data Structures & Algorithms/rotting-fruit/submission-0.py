2038c43fef8fddc2b041829d06dd56e81d09e22b
Ivan Korneychuk <ikorneyc@gmail.com>
2026-08-26 09:06:14 -0400

    Update: rotting-fruit - submission-0

--- Data Structures & Algorithms/rotting-fruit/submission-0.py
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -1,31 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        res = 0
-        fresh = 0
-        rows,cols = len(grid), len(grid[0])
         q = collections.deque()
-        dirs = [[-1,0],[1,0],[0,1],[0,-1]]
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r,c))
-                elif grid[r][c] == 1:
+        dirs = [(0,1),(1,0),(-1,0),(0,-1)]
+        rows, cols = len(grid),len(grid[0])
+        fresh = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    q.append((i,j))
+                elif grid[i][j] == 1:
                     fresh += 1
+                
+        time = 0
         while fresh > 0 and q:
-            for i in range(len(q)):
-                row, col = q.popleft()
+
+            for _ in range(len(q)):
+                rottenX, rottenY = q.popleft()
                 for dr,dc in dirs:
-                    r, c = row+dr,col+dc
+                    r,c = rottenX+dr,rottenY+dc
                     if r not in range(rows) or c not in range(cols) or grid[r][c] != 1:
                         continue
-                    else:
-                        q.append((r,c))
-                        grid[r][c] = 2
-                        fresh -= 1
-            res += 1
+                    grid[r][c] = 2
+                    fresh -= 1
+                    q.append((r,c))
 
             
-        return res if fresh == 0 else -1
+            time += 1
 
-                
+
+
+        return time if fresh == 0 else -1
