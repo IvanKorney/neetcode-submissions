36c4395accb043231b4e18ecda259610808b85a9
Ivan Korneychuk <ikorneyc@gmail.com>
2026-08-26 11:53:08 -0400

    Update: count-paths - submission-0

--- Data Structures & Algorithms/count-paths/submission-0.py
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -1,11 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def uniquePath(r,c):
-            if r == 0 and c == 0:
-                return 1
-            if r < 0 or c < 0:
-                return 0
-            left = uniquePath(r-1,c)
-            up = uniquePath(r,c-1)
-            return left + up
-        return uniquePath(n-1,m-1)
+        return int((math.factorial(m+n-2))/(math.factorial(n-1)*math.factorial(m-1)))
